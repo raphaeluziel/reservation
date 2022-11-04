e1bcd39e5c1df163f5453d6fa8c8a306ec3ba366
yiyuannew <49494825+dizzySummer@users.noreply.github.com>
2022-11-04 21:40:48 +0200

    tiny changes

--- reservation/settings/base.py
+++ reservation/settings/base.py
@@ -138,13 +138,22 @@ LOGIN_REDIRECT_URL = "index"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+#STATIC_URL = 'static/'
+
+
+#STATICFILES_DIRS=[
+   # os.path.join(BASE_DIR,'static')
+#]
+#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
+
+STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR/'static'
+
+
+
 
 MEDIA_URL= '/imgs/'
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'static')
-]
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 MEDIA_ROOT= BASE_DIR/'static/imgs'
 
 
