b2d2c822d076a904966081d77401fae54cd83cd7
yiyuannew <49494825+dizzySummer@users.noreply.github.com>
2022-11-05 22:21:27 +0200

    removed dead links

--- reservation/settings/base.py
+++ reservation/settings/base.py
@@ -137,20 +137,11 @@ LOGIN_REDIRECT_URL = "index"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-#STATIC_URL = 'static/'
-
-
-#STATICFILES_DIRS=[
-   # os.path.join(BASE_DIR,'static')
-#]
-#STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-
 STATIC_URL = '/static/'
-
-STATIC_ROOT = BASE_DIR/'static'
-
-
+STATIC_ROOT= 'staticfiles'
+STATICFILES_DIRS=[
+   os.path.join(BASE_DIR,'static')
+]
 
 
 MEDIA_URL= '/imgs/'
