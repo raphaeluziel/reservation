7b4860ab29c6f44fae439726151b1e32b7a8874a
yiyuannew <49494825+dizzySummer@users.noreply.github.com>
2022-12-04 22:27:19 +0200

    Update signals.py

--- core/signals.py
+++ core/signals.py
@@ -10,25 +10,30 @@ from django.core.mail import send_mail
 @receiver(pre_save, sender=Shift)
 def shift_pre_save_receiver(sender, instance,**kwargs):
     if instance.id is None:
-        print("This is from presave, I am creating shift now")
+        print(f"This is from presave, I am creating shift now,{sender},kwargs:{kwargs}")
+        
     else:
         previous = Shift.objects.get(id=instance.id)
-        print("Hello from pre-save, Shift id ",previous.id, "has been updated")
+        print(f"Hello from pre-save, Shift id ,{previous.id}, has been updated,{sender},kwargs:{kwargs}")
+        
 
 @receiver(post_save, sender=Shift)
 def shift_post_save_receiver(sender, instance,created,**kwargs):
     if created:
         id = instance.id
-        print("This is from post save, shift id", id,"has been created")
-        print("send email to blablabla")
+        print(f"This is from post save, shift {id} has been created,{sender},kwargs:{kwargs}")
+        print(f"send email to blablabla,{sender},kwargs:{kwargs}")
+        
+ 
     else:
         previous = Shift.objects.get(id=instance.id)
         print("This is from post save, Shift id ",previous.id, "has been updated")
+        print(f"{sender},kwargs:{kwargs}")
  
 
 @receiver(post_delete,sender=Shift)
 def delete_shift(sender,instance,**kwargs):
-    print("Shift has been deleted")
+    print(f"Shift has been deleted,{sender},kwargs:{kwargs}")
 
 
 
